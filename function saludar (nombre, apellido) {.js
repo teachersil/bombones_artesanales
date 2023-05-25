17afc14098207a9fa41e22f743ad5e0386f12f72
teachersil <silviacocilova@gmail.com>
2023-05-25 20:46:43 -0300

    agregue bootstrap

---
+++ function saludar (nombre, apellido) {.js
@@ -0,0 +1,5 @@
+function fav (nombre, apellido, serieFavorita){
+    alert ("Hola " + nombre + " " + apellido + ", tu serie favorita es: " + serieFavorita);
+}
+
+fav ("Harry", "Potter", "La Casa de Papel")
